e5d063934d2148b9843da25461fb20d94f58663c
Martin Richter <me@martinrichter.net>
2021-01-03 17:53:43 +0200

    Overhaul 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,17 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="Sivua ei löytynyt" />
+    <h1>Sivua ei löytynyt</h1>
+    <p>
+      Tarkista, että olet kirjoittanut osoitteen oikein. Voit myös{" "}
+      <Link to="/">palata aloitussivulle</Link>.
+    </p>
   </Layout>
 )
 
