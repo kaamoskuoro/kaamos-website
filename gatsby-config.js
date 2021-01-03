70679795131fd26a4b65dbe07eac9bdb2ef0cdab
Martin Richter <me@martinrichter.net>
2021-01-03 17:32:50 +0200

    Update site title and description
    Since we don’t have a Twitter handle to use as the author, we remove that metadata from the SEO component.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Kamarikuoro Kaamos`,
+    description: `Kaamos on vuonna 2007 perustettu helsinkiläinen kamarikuoro, jota johtaa Visa Yrjölä.`,
   },
   plugins: [
     `gatsby-plugin-sass`,
