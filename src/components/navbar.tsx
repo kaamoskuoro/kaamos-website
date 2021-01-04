2bd6ebcae1c5d8eadc5328c18e0522d3a1962fff
Martin Richter <me@martinrichter.net>
2021-01-04 15:33:52 +0200

    Add basic navbar and intro page

---
+++ src/components/navbar.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react"
+import { Link } from "gatsby"
+
+const Navbar: React.FC = props => {
+  const [isMenuExpanded, setMenuExpanded] = useState(false)
+
+  return (
+    <nav className="navbar is-primary">
+      <div className="container">
+        <div className="navbar-brand">
+          <NavbarBurger
+            isActive={isMenuExpanded}
+            onClick={() => setMenuExpanded(!isMenuExpanded)}
+          />
+        </div>
+        <div className={`navbar-menu ${isMenuExpanded ? "is-active" : ""}`}>
+          <div className="navbar-start">
+            <NavbarItem path="/" title="Etusivu" />
+            <NavbarItem path="/esittely/" title="Esittely" />
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+interface NavbarItemProps {
+  title: string
+  path: string
+}
+
+const NavbarItem: React.FC<NavbarItemProps> = props => (
+  <Link className="navbar-item" to={props.path}>
+    {props.title}
+  </Link>
+)
+
+interface NavbarBurgerProps {
+  isActive: boolean
+  onClick: () => void
+}
+
+const NavbarBurger: React.FC<NavbarBurgerProps> = props => (
+  <div
+    onClick={props.onClick}
+    className={`navbar-burger burger ${props.isActive ? "is-active" : ""}`}
+  >
+    <span />
+    <span />
+    <span />
+  </div>
+)
+
+export default Navbar
---
+++ src/pages/esittely.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Singers from "../../data/singers.json"
+
+const Esittely = () => (
+  <Layout>
+    <SEO title="Esittely" />
+    <h1>Esittely</h1>
+    <p>Welcome to page 2</p>
+  </Layout>
+)
+
+export default Esittely
