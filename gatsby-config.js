3761cced7d4ef9a86d27ae5ae7c08ac807e7addf
Martin Richter <me@martinrichter.net>
2021-01-04 16:02:39 +0200

    Remove some unused Gatsby features

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,5 @@ module.exports = {
     title: `Kamarikuoro Kaamos`,
     description: `Kaamos on vuonna 2007 perustettu helsinkiläinen kamarikuoro, jota johtaa Visa Yrjölä.`,
   },
-  plugins: [
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-react-helmet`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+  plugins: [`gatsby-plugin-sass`, `gatsby-plugin-react-helmet`],
 }
