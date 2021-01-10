305777e4587b4a59b02277131155d756119e82b7
Martin Richter <me@martinrichter.net>
2021-01-10 18:12:40 +0200

    Add social & streaming icon links

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,9 @@ module.exports = {
     title: `Kamarikuoro Kaamos`,
     description: `Kaamos on vuonna 2007 perustettu helsinkiläinen kamarikuoro, jota johtaa Visa Yrjölä.`,
   },
-  plugins: [`gatsby-plugin-sass`, `gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-fontawesome-css`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+  ],
 }
--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,11 +1,29 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import React from "react"
+import { faFacebook, faSpotify } from "@fortawesome/free-brands-svg-icons"
 
 import bannerImage from "../images/banner.png"
 
 const Banner: React.FC = () => (
   <div className="columns my-1">
-    <div className="column is-4">
+    <div className="column is-4 py-3">
       <img src={bannerImage} />
+    </div>
+    <div className="column is-clearfix py-3">
+      <div className="is-pulled-right">
+        <a
+          href="https://www.facebook.com/kamarikuorokaamos/"
+          className="icon is-large brand-facebook"
+        >
+          <FontAwesomeIcon icon={faFacebook} size="2x" />
+        </a>
+        <a
+          href="https://play.spotify.com/artist/2jrDYwEN5bxcYumwolQNu3"
+          className="icon is-large brand-spotify"
+        >
+          <FontAwesomeIcon icon={faSpotify} size="2x" />
+        </a>
+      </div>
     </div>
   </div>
 )
