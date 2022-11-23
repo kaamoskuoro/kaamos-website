c33fd88e4fc9140a57c1b4cb95251588aa5847ae
kaamos-admin <79757679+kaamos-admin@users.noreply.github.com>
2022-11-23 15:40:28 +0200

    Updated the copyright year (2011-2022)

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,7 +7,7 @@ const CMS_PATH = "/admin/"
 const Footer: React.FC = props => (
   <div className="content has-text-centered">
     <p className="has-text-grey">
-      &copy; Kamarikuoro Kaamos 2011–2021
+      &copy; Kamarikuoro Kaamos 2011–2022
       <br />
       <a href={CMS_PATH}>
         <FontAwesomeIcon icon={faFeatherAlt} /> CMS
