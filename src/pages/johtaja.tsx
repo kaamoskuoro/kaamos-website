7a8990093805aa0322cbf3ce3c710574c1589d8d
kaamos-admin <79757679+kaamos-admin@users.noreply.github.com>
2023-04-21 15:23:22 +0300

    Updated conductor biography

--- src/pages/johtaja.tsx
+++ src/pages/johtaja.tsx
@@ -13,24 +13,11 @@ const Johtaja: React.FC = () => (
         <h3 className="title is-3">Johtaja</h3>
 
         <p className="block">
-          <strong>Visa Yrjölä</strong> (s. 1992) valmistui musiikin maisteriksi
-          Taideyliopiston Sibelius-Akatemiasta kesäkuussa 2018 suoritettuaan
-          kuoronjohdon A-kurssitutkintonsa erinomaisin arvosanoin.
-          Kevätlukukaudella 2017 hän syvensi opintojaan Itävallassa Universität
-          für Musik und darstellende Kunst Wien -korkeakoulussa. Yrjölä on
-          osallistunut useille Euroopan ammattikamarikuorojen verkoston Tenson
-          järjestämille mestarikursseille työskennellen Cappella Amsterdamin,
-          Helsingin kamarikuoron, Alankomaiden kamarikuoron, Slovenian
-          Filharmonisen kuoron sekä Kroatian Radion ja television kuoron kanssa.
+          <strong>Visa Yrjölä</strong> valmistui musiikin maisteriksi Taideyliopiston Sibelius-Akatemiasta professori Nils Schweckendiekin kuoronjohtoluokalta vuonna 2018. Kevätlukukaudella 2017 hän syvensi opintojaan Itävallassa Universität für Musik und darstellende Kunst Wien -korkeakoulussa. Yrjölä on osallistunut useille Euroopan ammattikamarikuorojen verkoston Tenson järjestämille mestarikursseille työskennellen Cappella Amsterdamin, Helsingin kamarikuoron, Alankomaiden kamarikuoron, Slovenian Filharmonisen kuoron sekä Kroatian Radion ja television kuoron kanssa. Elokuussa 2018 hän osallistui myös Rundfunkchor Berlinin mestarikurssille.
         </p>
 
         <p className="block">
-          Kamarikuoro Kaamoksen lisäksi Visa Yrjölä johtaa kahta palkittua
-          mieskuoroa: Mieskuoro Eugaa sekä Kauppakorkeakoulun Ylioppilaskunnan
-          Laulajia KYL:liä. Vuoden 2013 Tampereen Sävelen kuorokatselmuksessa
-          hänelle myönnettiin nuoren ja lupaavan johtajan erikoispalkinto, ja
-          kesällä 2018 Bratislava Choir Festivalilla parhaan kuoronjohtajan
-          palkinto.
+          Kamarikuoro Kaamoksen lisäksi Visa Yrjölä johtaa kahta palkittua mieskuoroa: Mieskuoro Eugaa sekä Kauppakorkeakoulun Ylioppilaskunnan Laulajia KYL:liä. Lisäksi hän on vieraillut mm. Somnium Ensemblen ja Key Ensemblen kuoronjohtajana. Vuoden 2013 Tampereen Sävelen kuorokatselmuksessa Yrjölälle myönnettiin nuoren ja lupaavan johtajan erikoispalkinto, ja kesällä 2018 Bratislava Choir Festivalilla parhaan kuoronjohtajan palkinto. Maaliskuussa 2019 Radio Classic myönsi hänelle nuorten muusikoiden Tempo-palkinnon kapellimestari- / kuoronjohtajakategoriassa.
         </p>
 
         <img src={conductorImage} />
